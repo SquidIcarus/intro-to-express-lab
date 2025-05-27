57036e910610afc1034889d16d0fba1fbeee4cc4
User Name <landonisaiah@gmail.com>
2025-05-27 00:12:17 -0700

    exercise 1

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('<h1>Express server online</h1>'
+    )
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
+
+app.get('/greetings/:name', (req, res) => {
+    console.log(req.params.name);
+
+    res.send(`<h1>Hey ${req.params.name}, fancy seeing you here!`);
+});
