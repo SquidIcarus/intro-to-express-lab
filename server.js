cf1abfa2e9c659677f9bdf55e08c2267940bc675
User Name <landonisaiah@gmail.com>
2025-05-27 00:37:39 -0700

    exercise 2

--- server.js
+++ server.js
@@ -16,3 +16,13 @@ app.get('/greetings/:name', (req, res) => {
 
     res.send(`<h1>Hey ${req.params.name}, fancy seeing you here!`);
 });
+
+app.get('/roll/:diceNumber', (req, res) => {
+    console.log(req.params.diceNumber);
+    const diceNumber = req.params.diceNumber;
+    if (isNaN(diceNumber)) {
+        res.send(`<h1>You must specify a number, "${req.params.diceNumber}" is not a valid number!`);
+    } else {
+        res.send(`<h1>You rolled a ${req.params.diceNumber}`);
+    }
+});
