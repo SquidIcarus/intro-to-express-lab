efadf3c633c8aeefa88aaf12ea70d8d30f7866d4
User Name <landonisaiah@gmail.com>
2025-05-27 01:03:15 -0700

    exercise 3

--- server.js
+++ server.js
@@ -26,3 +26,21 @@ app.get('/roll/:diceNumber', (req, res) => {
         res.send(`<h1>You rolled a ${req.params.diceNumber}`);
     }
 });
+
+app.get('/collectibles/:indexNumber', (req, res) => {
+    const collectibles = [
+        { name: 'shiny ball', price: 5.95 },
+        { name: 'autographed picture of a dog', price: 10 },
+        { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+    ];
+
+    const index = parseInt(req.params.indexNumber);
+
+    if (isNaN(index) || index < 0 || index >= collectibles.length) {
+        return res.send(`This item is not yet in stock. Check back soon!`);
+    }
+
+    const item = collectibles[index];
+
+    res.send(`So, you want the ${item.name}? For $${item.price}, it can be yours!`);
+});
